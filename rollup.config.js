daf6824b870a9a4d17c972cc56a91f9362a28e6b
UrieliasCB <uri@corebooks.com>
2022-06-15 14:26:42 -0500

    Initial Configuration

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+import babel from "@rollup/plugin-babel";
+
+export default {
+  input: "./components/index.tsx",
+  output: { dir: "./lib", format: "cjs" },
+  external: ["rechart"],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/env", "@babel/preset-react"],
+    }),
+    ,
+    commonjs(),
+    typescript(),
+    postcss({
+      extensions: [".css"],
+    }),
+  ],
+};
