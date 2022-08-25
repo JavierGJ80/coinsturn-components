df039ee428110ff404a0ebec0a29558101752a35
JavierGJ <jgonzalez@coinsturn.com>
2022-08-25 12:49:58 -0500

    fine tunning

--- components/DonutGraph/index.tsx
+++ components/DonutGraph/index.tsx
@@ -18,34 +18,37 @@ export interface DonutGraphProps {
 
 const DonutGraph = (props: DonutGraphProps) => {
     const { COVER_value, COVER_color, BTC_value, BTC_color, ETH_value, ETH_color, USDT_value, USDT_color, hoverOffset} = props;
-    const graphData = {
-        labels: ["COVER", "BTC", "ETH", "USDT"],
-        datasets: [
-            {
-            label: 'Current crypto currency',
-            data: [COVER_value, BTC_value, ETH_value, USDT_value],
-            backgroundColor: [COVER_color, BTC_color, ETH_color, USDT_color],
-            borderColor: [COVER_color, BTC_color, ETH_color, USDT_color],
-            hoverOffset : hoverOffset,
-            },
-        ],
-    };
-    const options = {
-        plugins: {
-            legend: {
-                position: 'right',
-                rtl : true,
-                labels: {
-                usePointStyle: true,
-                pointStyle: "circle",
-                padding: 20,
+    // @ts-ignore
+    return(<Doughnut options={
+        {
+            plugins: {
+                legend: {
+                    position: 'right',
+                    rtl : true,
+                    labels: {
+                    usePointStyle: true,
+                    pointStyle: "circle",
+                    padding: 20,
+                    },
                 },
             },
-        },
-        cutout: "95%",
-    }
-    // @ts-ignore
-    return(<Doughnut options={options} data={ graphData }/>);
+            cutout: "95%",
+        }
+    } 
+    data={ 
+        {
+            labels: ["COVER", "BTC", "ETH", "USDT"],
+            datasets: [
+                {
+                label: 'Current crypto currency',
+                data: [COVER_value, BTC_value, ETH_value, USDT_value],
+                backgroundColor: [COVER_color, BTC_color, ETH_color, USDT_color],
+                borderColor: [COVER_color, BTC_color, ETH_color, USDT_color],
+                hoverOffset : hoverOffset,
+                },
+            ],
+        }
+    }/>);
 };
 
 export default DonutGraph;
