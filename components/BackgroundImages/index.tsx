2485567dab25f83da00fcd6e31017af07c8f92af
JavierGJ <jgonzalez@coinsturn.com>
2022-10-25 13:52:17 -0500

    background images

--- components/BackgroundImages/index.tsx
+++ components/BackgroundImages/index.tsx
@@ -17,25 +17,25 @@ const BackgroundImages = (props: BackgroundImagesProps) => {
 
     switch(selectedImage) {
         case("Home1"):{
-            return <img style={h1Styles} src={require("./HomeGifs/Home_1.gif")}></img>
+            return <img style={h1Styles} src={"https://github.com/JavierGJ80/coinsturn-components/blob/main/components/BackgroundImages/HomeGifs/Home_1.gif?raw=true"}></img>
         }
         case("Home2"):{
-            return <img style={h1Styles} src={require("./HomeGifs/Home_2.gif")}></img>
+            return <img style={h1Styles} src={"https://github.com/JavierGJ80/coinsturn-components/blob/main/components/BackgroundImages/HomeGifs/Home_2.gif?raw=true"}></img>
         }
         case("Home3"):{
-            return <img style={h1Styles} src={require("./HomeGifs/Home_3.gif")}></img>
+            return <img style={h1Styles} src={"https://github.com/JavierGJ80/coinsturn-components/blob/main/components/BackgroundImages/HomeGifs/Home_3.gif?raw=true"}></img>
         }
         case("Product1"):{
-            return <img style={h1Styles} src={require("./ProductGifs/Product_1.gif")}></img>
+            return <img style={h1Styles} src={"https://github.com/JavierGJ80/coinsturn-components/blob/main/components/BackgroundImages/ProductGifs/Product_1.gif?raw=true"}></img>
         }
         case("Product2"):{
-            return <img style={h1Styles} src={require("./ProductGifs/Product_2.gif")}></img>
+            return <img style={h1Styles} src={"https://github.com/JavierGJ80/coinsturn-components/blob/main/components/BackgroundImages/ProductGifs/Product_2.gif?raw=true"}></img>
         }
         case("Products"):{
-            return <img style={h1Styles} src={require("./ProductsGif/Products.gif")}></img>
+            return <img style={h1Styles} src={"https://github.com/JavierGJ80/coinsturn-components/blob/main/components/BackgroundImages/ProductsGif/Products.gif?raw=true"}></img>
         }
         default:{
-            return <img style={h1Styles} src={require("./HomeGifs/Home_1.gif")}></img>
+            return <img style={h1Styles} src={"https://github.com/JavierGJ80/coinsturn-components/blob/main/components/BackgroundImages/HomeGifs/Home_1.gif?raw=true"}></img>
         }
 
     }
