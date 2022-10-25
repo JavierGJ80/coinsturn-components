698231a562fb1796f33d7be84f159ba7f94768ac
Mario Medina Arriaga <mariomedinaarriaga@MacBook-Air-de-Mario.local>
2022-10-25 12:32:03 -0500

    gifs para backgrounds

--- components/BackgroundImages/index.tsx
+++ components/BackgroundImages/index.tsx
@@ -1,11 +1,46 @@
 import React from "react";
+import CSS from 'csstype';
+
+const h1Styles: CSS.Properties = {
+    backgroundColor: 'black',
+    width: '100%',
+    height: 'auto',
+  };
 
 export interface BackgroundImagesProps {
-
+    selectedImage : string;
 }
 
 const BackgroundImages = (props: BackgroundImagesProps) => {
-    return <span>Hello World!</span>
+    const { selectedImage } = props;
+    let gif : string;
+
+    switch(selectedImage) {
+        case("Home1"):{
+            return <img style={h1Styles} src={require("./HomeGifs/Home_1.gif")}></img>
+        }
+        case("Home2"):{
+            return <img style={h1Styles} src={require("./HomeGifs/Home_2.gif")}></img>
+        }
+        case("Home3"):{
+            return <img style={h1Styles} src={require("./HomeGifs/Home_3.gif")}></img>
+        }
+        case("Product1"):{
+            return <img style={h1Styles} src={require("./ProductGifs/Product_1.gif")}></img>
+        }
+        case("Product2"):{
+            return <img style={h1Styles} src={require("./ProductGifs/Product_2.gif")}></img>
+        }
+        case("Products"):{
+            return <img style={h1Styles} src={require("./ProductsGif/Products.gif")}></img>
+        }
+        default:{
+            return <img style={h1Styles} src={require("./HomeGifs/Home_1.gif")}></img>
+        }
+
+    }
 };
+
+
 
 export default BackgroundImages;
--- lib/components/BackgroundImages/index.d.ts
+++ lib/components/BackgroundImages/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="react" />
 export interface BackgroundImagesProps {
+    selectedImage: string;
 }
 declare const BackgroundImages: (props: BackgroundImagesProps) => JSX.Element;
 export default BackgroundImages;
