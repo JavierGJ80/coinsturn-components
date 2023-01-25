8796257cbb51152d2c30347f92ca4a34c2d50d6b
JavierGJ <jgonzalez@coinsturn.com>
2023-01-25 13:19:01 -0600

    merge resolve with dev

--- lib/components/TokenSpecific/index.d.ts
+++ lib/components/TokenSpecific/index.d.ts
@@ -2,12 +2,9 @@
 import "./index.css";
 import "@fontsource/poppins";
 export interface TokenSpecificProps {
-    backgroundColor: string;
-    fontColor: string;
     resPartner: [{
         [key: string]: any;
     }];
-    asset: string;
     theme: string;
 }
 declare const TokenSpecific: (props: TokenSpecificProps) => JSX.Element;
