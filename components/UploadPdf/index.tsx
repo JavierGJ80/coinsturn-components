58f63f0cae0fb7acf1f9102e8e9a6c4e95f846d4
JavierGJ <jgonzalez@coinsturn.com>
2023-02-28 21:32:41 -0600

    uploadPdf fine tuning

--- components/UploadPdf/index.tsx
+++ components/UploadPdf/index.tsx
@@ -12,22 +12,33 @@ const UploadPdf = (props: UploadPdfProps) => {
     const { resPartner, inputId, onChange } = props
     const language = resPartner[0].coinsturn_language
     const color = resPartner[0].coinsturn_theme
-    const [fileTitle, setFileTitle] = useState(language=="es"?"No hay archivos":"No file chosen")
+    const [fileTitles, setFileTitles] = useState([language=="es"?"No hay archivos":"No file chosen"])
 
     const onChangeEvent = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if(e.target.files!.length > 1){
-        setFileTitle(language=="es"?`(${e.target.files?.length}) archivos subidos`:`(${e.target.files?.length}) files uploaded`);
+      const newArr: string[]= []
+      if(e.target.files!.length == 0){
+        newArr.push(language=="es"?"No hay archivos":"No file chosen")
       }
       else{
-        setFileTitle(e.target.files![0].name);
+        Array.from(e.target.files!).map((file)=>{
+          newArr.push(file.name)
+        })
       }
+      setFileTitles(newArr);
       onChange(e.target.files);
     }
     return(
       <div className="uploadPdfCont">
         <input id={inputId} type={"file"} name="file" accept="application/pdf" onChange={onChangeEvent} hidden multiple/>
         <label className="fileInput" htmlFor={inputId}>{language=="es"?"Subir Archivos":"Chose Files"}</label>
-        <span id="file-chosen" style={{color:(color=="light"?"#000000":"#FFFFFF")}}>{fileTitle}</span>
+        {["No hay archivos", "No file chosen"].includes(fileTitles[0])?
+          <span id="file-chosen" style={{color:(color=="light"?"#000000":"#FFFFFF")}}>{fileTitles[0]}</span>:
+          <ul className="titleList" id="file-chosen-list" style={{color:(color=="light"?"#000000":"#FFFFFF")}}>
+            {fileTitles.map(file => (
+              <li key={file}>{file}</li>
+            ))}
+          </ul>
+        }
       </div>
     );
 };
